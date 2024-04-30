bdefcde1ab0b882b4191b7945935e064aab90223
Lord Crawford <lrc2161@columbia.edu>
2024-04-29 20:10:42 -0400

    more updates

--- src/components/home.js
+++ src/components/home.js
@@ -15,28 +15,33 @@ const Home = () => {
     // eventually, allow people to export the sounds (this sounds hard af to do but seems like a core feature)
     // allow them to record a segment or default shorter sound
 
+    function openInstructions() {
+        document.getElementById("instructions_overlay").classList.remove('hidden');
+    }
+
     return (
         <div id="home" className='h-screen text-white font-museomoderno'>
-            <div id="top" className='justify-between flex mt-[1.5%]'>
-                <h1 className='font-limelight w-[40%] pl-[2%] text-[4vw]' onClick={pageRefresh}>Everyday Sounds</h1>
-                {/* <img className="pl-[2%] w-[32%]" src={require('../images/logo.png')} alt="logo" onClick={pageRefresh}/> */}
-                <nav id="top-nav-right" className='flex justify-end items-center space-x-[20%] text-[1.5vw] mr-[3%]'>
-                    <button id="instructions_button">instructions</button>
+            <div id="top" className='justify-between flex mt-[1.5%] mt-[3vw]'>
+                <h1 className='font-limelight w-[40%] ml-[3%] text-[4vw]' onClick={pageRefresh}>EVERYDAY SOUNDS</h1>
+                <nav id="top-nav-right" className='flex flex-col text-[1.5vw] text-right mr-[3vw] mt-[1vw] space-y-[1vw]'>
+                    <button id="instructions_button" onClick={openInstructions}>instructions</button>
                     <a href = "#about">about</a>
                 </nav>
             </div>
-            <Player/>
-            <div id="sequencer" className='text-center mt-[5%]'>
-                <SoundRow soundrow_num={1}/>
-                <SoundRow soundrow_num={2}/>
-                <SoundRow soundrow_num={3}/>
-                <SoundRow soundrow_num={4}/>
-                <SoundRow soundrow_num={5}/>
-                <SoundRow soundrow_num={6}/>
-                <SoundRow soundrow_num={7}/>
-                <SoundRow soundrow_num={8}/>
-            </div>
             <Instructions/>
+            <div>
+                <Player/>
+                <div id="sequencer" className='text-center mt-[6%]'>
+                    <SoundRow soundrow_num={1}/>
+                    <SoundRow soundrow_num={2}/>
+                    <SoundRow soundrow_num={3}/>
+                    <SoundRow soundrow_num={4}/>
+                    <SoundRow soundrow_num={5}/>
+                    <SoundRow soundrow_num={6}/>
+                    <SoundRow soundrow_num={7}/>
+                    <SoundRow soundrow_num={8}/>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/instructions.js
+++ src/components/instructions.js
@@ -1,12 +1,28 @@
-import React, { useState} from 'react';
+import React from 'react';
 
 
 const Instructions = () => {
 
+    function close() {
+        document.getElementById("instructions_overlay").classList.add('hidden');
+    }
+    function outerClose(e){
+        if(e.target.id === 'instructions_overlay'){
+            document.getElementById("instructions_overlay").classList.add('hidden');
+        }
+    }
+
     return(
-        <div className='items-center flex justify-center text-black absolute top-[13%] text-[1.1vw] hidden'>
-            <div className='w-[50%] bg-white rounded-[0.5vw] p-[2%]'>
-                <h1 className='text-[5vw] font-limelight'>Manifesto</h1><br/>
+        <div id="instructions_overlay" className='flex justify-center text-black 
+            text-[1vw] hidden fixed w-[100%] h-screen top-0 right-0 left-0 bottom-0 bg-black bg-white/50'
+            onClick={outerClose}>
+            <div id="instructions_popup"className='animate-[fade-in_1s_ease-in-out] w-[50%] h-max bg-white rounded-[0.5vw] p-[2%] top-[13%] relative'>
+                <h1 className='text-[4.5vw] font-limelight'>Manifesto</h1><br/>
+                <button className='absolute top-[2%] right-[2%]' onClick={close}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="2vw" height="2vw" fill="currentColor" className="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
+                    </svg>
+                </button>
                 <span>What you see here is a 16-step sequencer. Sequencers allow you to program a combination of notes, 
 					rhythms, articulations and effects. Each row in this sequencer correlates to a different audio sound 
 					that you will import. <br/><br/>
@@ -16,7 +32,7 @@ const Instructions = () => {
 					which you can change) it will continue to the next step, until all 16 are played. Then it will loop as a 
 					regular 16-step pattern. <br/><br/><br/>
 
-					<u className='text-[1.5vw]'>Guide:</u><br/><br/>
+					<u className='text-[1.3vw]'>Guide:</u><br/><br/>
 
                     <ol className='list-[upper-roman] ml-[3%] space-y-[1%] w-[90%]'>
                         <li value="1">Choose an audio file that you'd like to use</li>
